2eeb141c20b91974f5e3ac1a8c0967365a0b967e
amoris123 <a_moris@fpzornotza.com>
2021-10-05 12:10:54 +0200

    header con mediaquery 2.0

---
+++ js/inicio.js
@@ -0,0 +1,7 @@
+/*
+var imgs=['img/facebook.png','img/insta.png','img/twitter.png','img/whats.png'];
+for (let i = 0; i < imgs.length; i++) {
+    document.getElementById('fot').innerHTML = document.getElementById('fot').innerHTML + '<div class="imagen"><p>' + (i + 1) + '</p><a  href="#" id="' + i + '" onclick="mostrar(' + i + ')"><img src="' + imgs[i] + '"/></a> </div>'
+  
+  }
+  */
