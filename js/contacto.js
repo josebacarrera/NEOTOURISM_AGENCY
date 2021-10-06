bb56c988897a9284d52960b6e56f4ad63fd2dd4c
josebacarrera <j_carrera@fpzornotza.com>
2021-10-06 12:56:42 +0200

    contacto en proceso

---
+++ js/contacto.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#Formulario").submit(function( event ){
+     event.preventDefault();
+
+     $.ajax({
+         type: 'POST',
+         url: '../php/contacto2.php',
+         data: $(this).serialize(),
+         success: function(data){
+             $("#respuesta").slideDown();
+             $("#respuesta").html(data);
+             $('#respuesta2').modal('show');
+             document.getElementById('Formulario').reset();
+         }
+     });
+
+     return false;
+ });
+});
+
+function enviarmensaje() {
+    window.alert("FORMULARIO ENVIADO");
+}
