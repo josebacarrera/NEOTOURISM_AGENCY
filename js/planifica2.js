5ca7237e426f559c1a8c0adc1cacb851358355d4
OnintzeA <74660608+OnintzeA@users.noreply.github.com>
2021-10-19 11:29:54 +0200

    coomit ajskldmzx

--- js/planifica2.js
+++ js/planifica2.js
@@ -1,5 +1,5 @@
 /*imagenes de seleccion lugar*/
-var select=['A','B','C','D'];
+var select=['1','2','3','4'];
 var imagen=['../img/a.jpg','../img/b.jpg','../img/c.jpg','../img/d.jpg'];
 
 /*info tabs*/
@@ -9,14 +9,20 @@ vuelos=[['NeoTokyo2D','Iberia','04:45','200€'],['NeoTokyo2D','Qatar Airways','
     'CyberPunk','Emirates','20:05','220€'],['CyberPunk','Iberia','06:35','800€'],['CyberPunk','Qatar Airways','17:55','1050€'],['CyberPunk','Lufthansa','22:00','130€'],[
     'Doraemon','Emirates','18:08','800€'],['Doraemon','Lufthansa','14:00','0€'],['Doraemon','Iberia','16:05','1500€'],['Doraemon','Qatar Airways','06:03','5600€']]
 
-hoteles=[['Hotel Grizzly','05:45','300€'],['Feliz estancia','21:35','700€'],['Hotel Spotlight','14:45','250€'],['Confort Total','03:00','1400€'],[
-    'Feliz estancia','07:55','910€'],['Hotel Grizzly','23:05','1300€'],['Confort Total','04:15','190€'],['Hotel Spotlight','07:40','4000€'],[
-    'Hotel Spotlight','22:05','240€'],['Hotel Grizzly','05:35','200€'],['Feliz estancia','16:55','1350€'],['Confort Total','23:00','140€'],[
-    'Hotel Spotlight','19:08','300€'],['Confort Total','13:00','11110€'],['Hotel Grizzly','15:05','100€'],['Feliz estancia','04:03','560€']]
+hoteles=[['hotel0','Hotel Grizzly','05:45','300€'],['hotel0','Feliz estancia','21:35','700€'],['hotel0','Hotel Spotlight','14:45','250€'],['hotel0','Confort Total','03:00','1400€'],[
+    'hotel1','Feliz estancia','07:55','910€'],['hotel1','Hotel Grizzly','23:05','1300€'],['hotel1','Confort Total','04:15','190€'],['hotel1','Hotel Spotlight','07:40','4000€'],[
+    'hotel2','Hotel Spotlight','22:05','240€'],['hotel2','Hotel Grizzly','05:35','200€'],['hotel2','Feliz estancia','16:55','1350€'],['hotel2','Confort Total','23:00','140€'],[
+    'hotel3','Hotel Spotlight','19:08','300€'],['hotel3','Confort Total','13:00','11110€'],['hotel3','Hotel Grizzly','15:05','100€'],['hotel3','Feliz estancia','04:03','560€']]
+
+actividades=[['actividad0','actividad Grizzly','05:45','300€'],['actividad0','Feliz estancia','21:35','700€'],['actividad0','actividad Spotlight','14:45','250€'],['actividad0','Confort Total','03:00','1400€'],[
+    'actividad1','Feliz estancia','07:55','910€'],['actividad1','actividad Grizzly','23:05','1300€'],['actividad1','Confort Total','04:15','190€'],['actividad1','actividad Spotlight','07:40','4000€'],[
+    'actividad2','actividad Spotlight','22:05','240€'],['actividad2','actividad Grizzly','05:35','200€'],['actividad2','Feliz estancia','16:55','1350€'],['actividad2','Confort Total','23:00','140€'],[
+    'actividad3','actividad Spotlight','19:08','300€'],['actividad3','Confort Total','13:00','11110€'],['actividad3','actividad Grizzly','15:05','100€'],['actividad3','Feliz estancia','04:03','560€']]
+
 
 for (let index = 0; index < select.length; index++) {
     document.getElementById('seleccion').innerHTML 
-    += '<div id="'+select[index]+'" class="img img-sitio border border-2 rounded-3 border-primary m-auto my-5 d-block"><img class="w-100 h-100" src="'+imagen[index]+'"></div>';
+    += '<div id="'+select[index]+'" class="bg-image hover-zoom img img-sitio border border-2 rounded-3 border-primary m-auto my-5 d-block "><h2>'+vuelos[0]+'</h2><img class="w-100 h-100" src="'+imagen[index]+'"></div>';
 }
 
 /*tabs segun seleccion*/
@@ -31,68 +37,155 @@ borrar();
        
         imagenSup=document.querySelectorAll('.img-sitio');
         //imagenSup.forEach(element => element.addEventListener('click', function () {}));//cierre imagenSup foreach
-            
+          
         for (let index = 0; index < imagenSup.length; index++) {
-           imagenSup(index).addEventListener('click', function(){sacarDatosGeneral(i)});            
+           imagenSup[index].addEventListener('click', function(){sacarDatosGeneral(index)});
+           console.log(index);               
         }
-        
-/****************************************************************************************************************************************************************************************** */
-            
+       
 miArray="vuelos";
 resul=eval(miArray);
-  
+console.log(resul);
 
- function sacarDatosGeneral(indice) {
-     miArray="";
-           if(indice==0){
-                miArray="vuelos";
-         }else{
-                miArray="hoteles";
-         }
-     resul=eval(miArray)
-     console.log(resul)
-     sacarDestino(destino,resul)   
+ function sacarDatosGeneral(index) {
+
+    borrar();
+    document.getElementById("idt" + index).style.display = "block";
+    document.getElementById("tabuno" + index).addEventListener("click", function () {generarInfo("1", index)});
+    document.getElementById("tabdos" + index).addEventListener("click", function () {generarInfo("2", index)});
+    document.getElementById("tabtres" + index).addEventListener("click", function () {generarInfo("3", index)});
      
+ }
+ 
+function generarInfo(info, index){
+    document.getElementById("autoGen").style.display = "block";
+    document.getElementById("autoGen").innerHTML = "";
 
- } 
+    if (info == 1) { //Vuelos
 
-function sacarDestino(destino,resul){
-       lista="";
-       elementoLista="";
-       for(i=0;i<resul.length;i++){
-          destino=destino.trim();
-         if((destino=="NeoTokyo2D") || (destino==resul[i][0])){
-             console.log(resul[i][0]+"-"+destino)
-             elementoLista="<p>"+ resul[i][0]+"-"+resul[i][1]+"-"+resul[i][2]+"-"+resul[i][3]+"-"+"</p>";
-             lista=lista+elementoLista;
-          }
-        }
-     document.querySelector('#vuelosHoteles').innerHTML=lista;
-  }
-  
+        vuelos.forEach(element => {
+            
+            if (index == 0 && element[0] == "NeoTokyo2D") { //Datos del idt0
 
-/******************************************************************************************************************************************************************************************* */
-        /*document.getElementById("a").addEventListener("click", function(){
-            borrar();
-            document.getElementById('idta').style.display='block';
-            document.getElementsByClassName('tab').innerHTML
-            +='<div class="entrada impar"> <h3 class="compañia w-100 mx-5">Vueling</h3> <h3 class="hora w-50">2h</h3>  <h3 class="precio w-25">300€</h3> <button class="btnS">Comprar</button> </div>';
-        });//a cierre
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
 
-        
-        document.getElementById("b").addEventListener("click", function(){
-            borrar();
-            document.getElementById('idtb').style.display='block';
-        });//b cierre
+            if (index == 1 && element[0] == "Akira") { //Datos del idt1
 
-    
-        /document.getElementById("c").addEventListener("click", function(){
-            borrar();
-            document.getElementById('idtc').style.display='block';
-        });//c cierre
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
 
-        document.getElementById("d").addEventListener("click", function(){
-            borrar();
-            document.getElementById('idtd').style.display='block';
-        });//d cierre  */ 
-  });//addEventListener("DOMContentLoaded"
+            if (index == 2 && element[0] == "CyberPunk") { //Datos del idt2
+
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
+
+            if (index == 3 && element[0] == "Doraemon") { //Datos del idt3
+
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
+        });
+
+    }//cierre vuelo autogen
+
+    if (info == 2) { //Hoteles
+        hoteles.forEach(element => {
+            
+            if (index == 0 && element[0] == "hotel0") { //Datos del idt0
+
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
+
+            if (index == 1 && element[0] == "hotel1") { //Datos del idt1
+
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
+
+            if (index == 2 && element[0] == "hotel2") { //Datos del idt2
+
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
+
+            if (index == 3 && element[0] == "hotel3") { //Datos del idt3
+
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
+        });
+    }//cierre autogen hoteles
+
+    if (info == 3) { //Actividades
+        actividades.forEach(element => {
+            
+            if (index == 0 && element[0] == "actividad0") { //Datos del idt0
+
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
+
+            if (index == 1 && element[0] == "actividad1") { //Datos del idt1
+
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
+
+            if (index == 2 && element[0] == "actividad2") { //Datos del idt2
+
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
+
+            if (index == 3 && element[0] == "actividad3") { //Datos del idt3
+
+                document.getElementById("autoGen").innerHTML += "<div class='entrada impar'>"
+                                                             +  "<h3 class='compañia w-100 mx-5'>"+element[1]+"</h3>"
+                                                             +  "<h3 class='hora w-50'>"+element[2]+"</h3>"
+                                                             +  "<h3 class='precio w-25'>"+element[3]+"</h3>"
+                                                             +  "<button class='btnS'>Comprar</button></div>"
+            }
+        });
+    }//cierre autogen hoteles
+
+ }//CIERRE AUTOGEN TODO
+
+});//addEventListener("DOMContentLoaded"
