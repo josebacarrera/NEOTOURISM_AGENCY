a4e9641ba282e15e029d5de3bc6bf4558c1793ba
ekaitz1337 <ekaitz.alvarez34@gmail.com>
2021-10-19 12:08:37 +0200

    x

--- js/style.js
+++ js/style.js
@@ -1,9 +1,7 @@
 //BOTÓN SUSCRITO
 function suscrito(evObject) {
-    console.log("vdbda")
     evObject.preventDefault();
     var nombre = document.getElementById("correoo")
-    console.log(nombre);
     if (nombre.value == null || nombre.value.length == 0) {
         alert("Escribe un correo,el campo no puede estar vacio");
          return false;
